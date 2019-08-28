aab62926e6a3d56ff4d067ca5156b1e7a7119350
Randoragon <randoragongamedev@gmail.com>
2019-08-28 17:47:23 +0200

    Add example.py
    example.py contains some simple interactive code for drawing B-Splines,
    I've updated .gitignore to bring that file to the repository, as it's
    yet another way of illustrating the capabilities of the bashdraw module.

---
+++ example.py
@@ -0,0 +1,63 @@
+from os import system
+import bashdraw as bd
+import keyboard
+
+g = bd.Display(21, 21, bd.LEFT)
+g.NewState('A')
+g.SetState('A')
+# g.DrawFigure(bd.Rectangle(0, 0, 21, 21, 'blue'))
+# g.DrawFigure(bd.Line(1, 1, 19, 19, 'cyan'))
+# g.DrawFigure(bd.Line(19, 1, 1, 19, 'cyan'))
+# g.DrawFigure(bd.Line(10, 1, 10, 19, 'cyan'))
+# g.DrawFigure(bd.Line(1, 10, 19, 10, 'cyan'))
+# g.DrawFigure(bd.Line(5, 1, 15, 19, 'cyan'))
+# g.DrawFigure(bd.Line(15, 1, 5, 19, 'cyan'))
+# g.DrawFigure(bd.Line(1, 5, 19, 15, 'cyan'))
+# g.DrawFigure(bd.Line(1, 15, 19, 5, 'cyan'))
+# g.DrawFigure(bd.Rectangle(9, 9, 3, 3, 'white'))
+
+# g.DrawFigure(bd.Triangle(4, 4, 16, 8, 8, 16, True, 'red'))
+def addX(val, num):
+    global g
+    if not ((val.X == 0 and num == -1) or (val.X == g.dim.W - 1 and num == 1)):
+        val.X += num
+    redraw()
+    return True
+def addY(val, num):
+    global g
+    if not ((val.Y == 0 and num == -1) or (val.Y == g.dim.H - 1 and num == 1)):
+        val.Y += num
+    redraw()
+    return True
+def setsel(val):
+    global sel, p
+    p[sel] = bd.Point(p[sel].X, p[sel].Y, 'cyan')
+    sel = val
+    p[sel] = bd.Point(p[sel].X, p[sel].Y, 'yellow')
+    redraw()
+    return True
+p = [bd.Point(1, 1, 'yellow'), bd.Point(8, 20, 'cyan'), bd.Point(12, 3, 'cyan'), bd.Point(15, 15, 'cyan'), bd.Point(18, 18, 'cyan')]
+sel = 0
+keyboard.add_hotkey('1', lambda: setsel(0))
+keyboard.add_hotkey('2', lambda: setsel(1))
+keyboard.add_hotkey('3', lambda: setsel(2))
+keyboard.add_hotkey('4', lambda: setsel(3))
+keyboard.add_hotkey('5', lambda: setsel(4))
+keyboard.add_hotkey('h', lambda: addX(p[sel], -1))
+keyboard.add_hotkey('j', lambda: addY(p[sel], 1))
+keyboard.add_hotkey('k', lambda: addY(p[sel], -1))
+keyboard.add_hotkey('l', lambda: addX(p[sel], 1))
+def redraw():
+    g.Clear()
+    g.DrawFigure(bd.Rectangle(0, 0, g.dim.W, g.dim.H, 'grey'))
+    g.DrawFigure(bd.Spline(p[0], p[1], p[2], p[3], p[4], 'red'))
+    g.DrawFigure(p[0])
+    g.DrawFigure(p[1])
+    g.DrawFigure(p[2])
+    g.DrawFigure(p[3])
+    g.DrawFigure(p[4])
+    system('cls')
+    g.Draw()
+redraw()
+while True:
+    pass
